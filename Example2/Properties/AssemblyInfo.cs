cca3c34e26bb05f60e63622d5263d75fa76b3dcf
Alexei Bozhenov <aleksei@upnest.com>
2022-10-14 23:21:57 +0600

    Добавьте файлы проекта.

---
+++ Example2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Example2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Example2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e7f6e42-078d-4285-a25c-e082fca46e31")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Example2/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+using NUnit.Framework;
+using NSelene;
+using static NSelene.Selene;
+
+namespace Example2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new ChromeOptions();
+            options.AddArguments("--lang=ru-ru");
+            driver = new ChromeDriver(options);
+            wait = new WebDriverWait(driver, new TimeSpan(0, 4, 0));
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://www.google.ru/";
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector("[name = 'q']")));
+            driver.FindElement(By.CssSelector("[name = 'q']")).SendKeys("Selenium");
+            //driver.FindElement(By.CssSelector("[class = 'FPdoLc lJ9FBc'] [name = 'btnK']")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//div[5]/center/input[1]")));
+            driver.FindElement(By.XPath("//div[5]/center/input[1]")).Click();
+            Thread.Sleep(3000);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            driver.Url = "https://www.google.ru/";
+            S("[name = 'q']", driver).Should(Be.Visible).SetValue("Selenium");
+            S("//div[5]/center/input[1]", driver).Should(Be.Visible).Click();
+            Thread.Sleep(3000);
+            
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
